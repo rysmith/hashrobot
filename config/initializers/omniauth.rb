c7f7c1d3d913ae9a35325ffae06a231885201f7c
rysmith <ryansmith05@gmail.com>
2015-06-12 13:39:16 -0700

    oauth integrated, issue #13, [#96836370]

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+
+  OmniAuth.config.on_failure = Proc.new do |env|
+    SessionsController.action(:auth_failure).call(env)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback' => 'sessions#create', as: :twitter
 
+  delete '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
