0c7c04deb05f30c164690fa7bffebc9256c736ab
rysmith <ryansmith05@gmail.com>
2015-06-15 18:45:35 -0700

    start monkeylearn API integration, issue #28, [#96959514]

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,26 @@
 class CategoriesController < ApplicationController
   def index
+
+    monkey_token = Figaro.env.monkey_learn_token
+
+    uri = URI.parse("https://api.monkeylearn.com/v2/classifiers/cl_5icAVzKR/classify/")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Post.new(uri.request_uri)
+
+    # Set POST data
+    request.body = {
+        text_list:
+            [
+              "I am so happy to announce my new partnership with Pantene! #SelenaxPantene #strongisbeautiful @Pantene",
+              "Alarm is mounting in South Korea over Middle East Respiratory Syndrome http://econ.st/1HLyfCu"
+            ]}.to_json
+
+    # Set headers
+    request.add_field("Content-Type", "application/json")
+    request.add_field("Authorization", "token #{monkey_token}")
+
+    render text: http.request(request).body
   end
 
   def show
