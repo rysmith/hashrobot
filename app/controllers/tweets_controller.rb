fe0055c18180d269287d8d4408490288ba6c99b7
jeffdemers <jeffdemers@gmail.com>
2015-06-17 16:17:02 -0700

    added grackle gem to store tweets in db, issue #10, [#96836832]

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,10 @@
 class TweetsController < ApplicationController
 
   def index
-    puts $client.status(167309659198328832).text
-    puts $client.search("to:justinbieber marry me", result_type: "recent").take(3).collect do |tweet|
-      "#{tweet.user.screen_name}: #{tweet.text}"
-    end
+    @tweets = Tweet.all
   end
 
-  def show
-  end
+def show
+end
+
 end
