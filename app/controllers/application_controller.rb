c6e8f4ddcda4a8138872b4e335188a73888a5ce7
rysmith <ryansmith05@gmail.com>
2015-06-18 14:37:38 -0700

    use twitter gem instead of Grackel to save tweets, issue #10, [#96913196]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
-
-
-  $client = Twitter::REST::Client.new do |config|
-    config.consumer_key = Figaro.env.twitter_key
-    config.consumer_secret = Figaro.env.twitter_secret
-    config.access_token = Figaro.env.access_token
-    config.access_token_secret = Figaro.env.access_token_secret
-  end
-
-  require 'twitter'
-
-  private
+private
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,15 +1,11 @@
-require 'grackle'
 require 'twitter'
-require 'rubygems'
 
 class Tweet < ActiveRecord::Base
 
-  MY_APPLICATION_NAME = "jdlb"
-
-  """Connect to the Twitter API and pull down the latest tweets"""
   def self.get_latest
-    tweets = client.statuses.user_timeline? :screen_name => MY_APPLICATION_NAME # hit the API
-    puts tweets
+
+    tweets = client.search("los angeles", result_type: "recent", language: "en", count: 1)
+
     tweets.each do |t|
       # Tweet.create({:content => t.text, :created => created })
       # created = DateTime.parse(t.created_at)
@@ -21,15 +17,16 @@ class Tweet < ActiveRecord::Base
   end
 
 
-  private
+private
+
   def self.client
-    Grackle::Client.new(:ssl=>true,:auth =>{
-                            :type => :oauth,
-                            :consumer_key => Figaro.env.twitter_key,
-                            :consumer_secret => Figaro.env.twitter_secret,
-                            :token => Figaro.env.access_token,
-                            :token_secret => Figaro.env.access_token_secret
-                        })
+
+    Twitter::REST::Client.new do |config|
+      config.consumer_key = Figaro.env.twitter_key
+      config.consumer_secret = Figaro.env.twitter_secret
+      config.access_token = Figaro.env.access_token
+      config.access_token_secret = Figaro.env.access_token_secret
+    end
 
   end
 end
