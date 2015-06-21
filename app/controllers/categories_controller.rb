80c3f5b2e14343b65c4344df718eb89beebf69ed
rysmith <ryansmith05@gmail.com>
2015-06-20 17:35:16 -0700

    pass data from tweet table to monkeylearn api, issue #28, [#96959514]

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
   def index
-
+    @tweets = Tweet.all.order(created: :desc)
   end
 
 
@@ -10,6 +10,15 @@ class CategoriesController < ApplicationController
   end
 
   def create
+
+    text_list = []
+
+    @tweets = Tweet.all.order(created: :desc)
+
+    @tweets.each do |t|
+      text_list << t['content']
+    end
+
     monkey_token = Figaro.env.monkey_learn_token
 
     #cl_5icAVzKR is the generic topic classifier
@@ -19,18 +28,18 @@ class CategoriesController < ApplicationController
     request = Net::HTTP::Post.new(uri.request_uri)
 
     # Set POST data
-    # request.body = {
-    #     text_list:
-    #         [
-    #             "Text to analyze",
-    #             "Second text to analyze"
-    #         ]}.to_json
-    #
-    # # Set headers
-    # request.add_field("Content-Type", "application/json")
-    # request.add_field("Authorization", "token #{monkey_token}")
+    request.body = {
+        text_list:
+            text_list
+    }.to_json
+
+    # Set headers
+    request.add_field("Content-Type", "application/json")
+    request.add_field("Authorization", "token #{monkey_token}")
 
     @response = http.request(request).body
+
+    puts @response
 
     redirect_to categories_path
 
