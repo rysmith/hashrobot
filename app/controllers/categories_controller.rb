cc9b418f4b1545c00b4acf49b4394572423e1d07
rysmith <ryansmith05@gmail.com>
2015-06-22 17:24:18 -0700

    associate a tweet with a category, closes #51, [#96959514]

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,7 +14,11 @@ class CategoriesController < ApplicationController
 
     all_tweets = compile_tweets(Tweet)
 
-    tweet_category = analyze_tweets(all_tweets)
+    tweet_id = compile_tweet_id(Tweet)
+
+    response = analyze_tweets(all_tweets)
+
+    tweet_category = inject_tweet_id(response, tweet_id)
 
     save_tweets(tweet_category)
 
@@ -47,6 +51,25 @@ private
     tweet_content
   end
 
+  # returns an array of integers (id of the tweets in the tweet table)
+  def compile_tweet_id(tweets_table)
+
+    # create an empty array to store the id of each tweet
+    tweet_id = []
+
+    # get all the tweets from the db
+    tweets = tweets_table.all.order(created: :desc)
+
+    # extract the id for each tweet and push it to tweet_id
+    tweets.each do |tweet|
+
+      tweet_id << tweet['id']
+    end
+
+    # return the id of each tweet
+    tweet_id
+  end
+
   # analyse_tweets expects an array of strings
   # returns a JSON object
   def analyze_tweets(tweet_list)
@@ -70,23 +93,32 @@ private
     # pass in the monkeylearn API token in the header
     request.add_field("Authorization", "token #{monkey_token}")
 
-    # return the body of the response
-    http.request(request).body
+    # parse the monkeylearn response to usable JSON
+    response = JSON.parse http.request(request).body
+  end
+
+  # once the tweets have been processed
+  def inject_tweet_id (response, tweet_id)
+
+    i = 0
+
+    response['result'].each do |t|
+
+      t[0].merge!("id" => tweet_id[i])
+      i += 1
+    end
   end
 
   # save the results from the monkeylearn API to the categories table
   def save_tweets(analyzed_tweets)
 
-    #parse the monkeylearn response usable JSON
-    hash_tweets =  JSON.parse analyzed_tweets
-
     #extract the arrays of categories
-    result = hash_tweets['result']
+    result = analyzed_tweets
 
     #use only the first category result for each tweet and save it to the categories table
     result.each do |t|
 
-      Category.create({:probability => t[0]['probability'], :category => t[0]['label'] })
+      Category.create({:probability => t[0]['probability'], :category => t[0]['label'], :tweet_id => t[0]['id'] })
     end
   end
 end
