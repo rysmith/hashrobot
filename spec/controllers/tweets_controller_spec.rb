917c9feb9f75789ddacd79dc667a80036ef55fa7
jeffdemers <jeffdemers@gmail.com>
2015-06-25 17:04:11 -0700

    removed routes testing

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -1,21 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe TweetsController, :type => :controller do
-  it 'should get the tweets (index) page' do
-
-    get :index
-
-    expect(response).to have_http_status(:success)
-
-    expect(response).to render_template(:index)
-  end
-
-  it 'should get the new tweet page' do
-
-    get :new
-
-    expect(response).to have_http_status(:success)
-
-    expect(response).to render_template(:new)
-  end
 end
